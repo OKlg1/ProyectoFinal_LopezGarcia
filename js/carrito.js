2af253714d5bdbeb8f41a2ad0a6e93f7c73f52e7
Kristel López <olga.kristel.l@gmail.com>
2023-10-16 21:00:40 -0600

    first commit

---
+++ js/carrito.js
@@ -0,0 +1,78 @@
+const pintarCarrito = () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+    modalHeader.innerHTML = `
+    <h2 class="modal-header-title">Carrito.</h2>
+`;
+    modalContainer.append(modalHeader);
+
+    const modalbutton = document.createElement("h2");
+    modalbutton.innerText = "X";
+    modalbutton.className = "modal-header-button";
+
+    modalbutton.addEventListener("click", () => {
+        modalContainer.style.display = "none";
+    });
+
+    modalHeader.append(modalbutton);
+
+    carrito.forEach((product) => {
+        let carritoContent = document.createElement("div")
+        carritoContent.className = "modal-content"
+        carritoContent.innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p>$${product.precio}</p>
+        <p>Cantidad: ${product.cantidad}</p>
+        `;
+
+        modalContainer.append(carritoContent)
+
+        let eliminar = document.createElement("span");
+        eliminar.innerText = "X";
+        eliminar.className = "delete-product";
+        carritoContent.append(eliminar);
+
+        eliminar.addEventListener("click", eliminarProducto);
+    });
+
+    const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
+
+    const totalBuying = document.createElement("div")
+    totalBuying.className = "total-content"
+    totalBuying.innerHTML = `Total a pagar: $${total}`;
+    modalContainer.append(totalBuying);
+};
+
+verCarrito.addEventListener("click", pintarCarrito);
+
+const eliminarProducto = () => {
+    const foundId = carrito.find((element) => element.id);
+
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== foundId;
+    });
+    carritoCounter();
+    pintarCarrito();
+};
+
+const carritoCounter = () => {
+    cantidadCarrito.style.display = "block";
+    cantidadCarrito.innerText = carrito.length
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/products.js
@@ -0,0 +1,37 @@
+const productos = [
+    {
+         id: 1, 
+         nombre: "Fresas", 
+         precio: 80, 
+         img: "https://img.interempresas.net/fotos/1341264.jpeg",
+         cantidad: 1,
+    },
+    { 
+        id: 2, 
+        nombre: "Limones", 
+        precio: 30,  
+        img: "https://marketingstorming.files.wordpress.com/2014/09/limones_marketingstorming_aklof.jpg?w=525", 
+        cantidad: 1,
+    },
+    { 
+        id: 3, 
+        nombre: "Naranjas", 
+        precio: 25,  
+        img: "https://www.ammarket.com/wp-content/uploads/2021/12/NARANJA_MESA_AMMARKET.COM_5.jpg", 
+        cantidad: 1,
+    },
+    { 
+        id: 4, 
+        nombre: "Uvas", 
+        precio: 120,  
+        img: "https://frutasolivar.com/wp-content/uploads/2020/05/40010140_s.jpg", 
+        cantidad: 1,
+    },
+    { 
+        id: 5, 
+        nombre: "Manzanas", 
+        precio: 40,  
+        img: "https://biotrendies.com/wp-content/uploads/2015/06/manzana.jpg", 
+        cantidad: 1,
+    },
+];
